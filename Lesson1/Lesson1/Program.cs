ff72c3661dfce6a1d9dc20df4a464edfbb77628f
laura.abukauskaite@visma.com <laura.abukauskaite@visma.com>
2016-06-20 18:37:14 +0300

    Bla bla bla

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kokia darbuotojo darbo patirtis?");
+
+            string input = Console.ReadLine();
+
+            double experience = double.Parse(input);
+
+            if (experience == 0)
+            {
+                Console.WriteLine("Entry");
+            }
+            else if (experience > 0 && experience < 2)
+            {
+                Console.WriteLine("Junior");
+            }
+            else if (experience >= 2 && experience < 4)
+            {
+                Console.WriteLine("Mid-level");
+
+            }
+            else if (experience >= 4 && experience < 6.5)
+            {
+                Console.WriteLine("Professional");
+                
+            }
+            else if (experience >6.5)
+            {
+                Console.WriteLine("Senior");
+            
+            }
+            else 
+            {
+                Console.WriteLine("Nesamone");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
