0b542a23d1127f3d3227f2b9e1460d2f6f4b3688
laura.abukauskaite@visma.com <laura.abukauskaite@visma.com>
2016-06-21 21:51:53 +0300

    - fourth task

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -10,50 +10,57 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kokia darbuotojo darbo patirtis?");
+            for (int i = 0; i < 100; i++)
+            {
 
-            string input = Console.ReadLine();
-            string[] competence = new[] { "Entry", "Junior", "Mid-level", "Professional", "Senior"};
-            double experience;
-            if (double.TryParse(input, out experience)== false)
-            {
-                Console.WriteLine("Klaida: Ivestas blogas argumentas. Ivesti galima tik skaiciu.");
-                Console.ReadLine();
-                return;
-            
-            }
-                
 
-            if (experience == 0)
-            {
-                Console.WriteLine(competence[0]);
-            }
-            else if (experience > 0 && experience < 2)
-            {
-                Console.WriteLine(competence[1]);
-            }
-            else if (experience >= 2 && experience < 4)
-            {
-                Console.WriteLine(competence[2]);
+                Console.WriteLine("Kokia darbuotojo darbo patirtis?");
 
-            }
-            else if (experience >= 4 && experience < 6.5)
-            {
-                Console.WriteLine(competence[3]);
-                
-            }
-           
-            else if (experience <0 || experience >100)
-            {
-                Console.WriteLine("Klaida: Darbo patirtis metais negali buti mazesne nei 0 arba didesne nei 100.");
-            }
-            else if (experience > 6.5)
-            {
-                Console.WriteLine(competence[4]);
+                string input = Console.ReadLine();
+                string[] competence = new[] { "Entry", "Junior", "Mid-level", "Professional", "Senior" };
 
+                double experience;
+                if (double.TryParse(input, out experience) == false)
+                {
+                    Console.WriteLine("Klaida: Ivestas blogas argumentas. Ivesti galima tik skaiciu.");
+                   
+                    continue;
+
+                }
+
+
+                if (experience == 0)
+                {
+                    Console.WriteLine(competence[0]);
+                }
+                else if (experience > 0 && experience < 2)
+                {
+                    Console.WriteLine(competence[1]);
+                }
+                else if (experience >= 2 && experience < 4)
+                {
+                    Console.WriteLine(competence[2]);
+
+                }
+                else if (experience >= 4 && experience < 6.5)
+                {
+                    Console.WriteLine(competence[3]);
+
+                }
+
+                else if (experience < 0 || experience > 100)
+                {
+                    Console.WriteLine("Klaida: Darbo patirtis metais negali buti mazesne nei 0 arba didesne nei 100.");
+                }
+                else if (experience > 6.5)
+                {
+                    Console.WriteLine(competence[4]);
+
+                }
             }
             Console.ReadLine();
         }
+        
 
     }
 }
